7814ae438edfbe4544d96533443466c450cf9393
BhagyashriSabale <balisabale16@gmail.com>
2023-05-17 19:08:28 +0530

    [Bhagyashri] Enter Valid First Name

--- Program.cs
+++ Program.cs
@@ -5,6 +5,28 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome User Registration Problem");
+
+            Console.Write("Enter First Name: ");
+            String firstName = Console.ReadLine();
+            if (IsValidFirstName(firstName))
+            {
+                Console.WriteLine("First name is valid.");
+            }
+            else
+            {
+                Console.WriteLine("First Name is not valid.");
+            }
+        }
+        static bool IsValidFirstName(string firstName)
+        {
+            if(firstName.Length >=3 && char.IsUpper(firstName[0])) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
