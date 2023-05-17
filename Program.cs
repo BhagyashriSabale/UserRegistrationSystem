245db9fe72520a385d4dbc2fa2ca247fda9e1cfc
BhagyashriSabale <balisabale16@gmail.com>
2023-05-17 18:58:12 +0530

    [Bhagyashri] welcome User Registration Program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistrationSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome User Registration Problem");
+        }
+    }
+}
